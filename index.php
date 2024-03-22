fc804228d1fb622164643f41002ab96388b45738
Revoqo <130451685+Oliwer22@users.noreply.github.com>
2024-03-22 10:28:10 +0100

    KRASHOSTING

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style.css">
+    <title>Kras Hosting</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="logo.png" alt="Logo">
+        </div>
+        <nav >
+            <ul >
+                <li><a href="#" class='active'>HOME</a></li>
+                <li><a href="#" class='links'>ABOUT</a></li>
+                <li><a href="#"class='links'>PRODUCTS</a></li>
+                <li><a href="#"class='links'>CONTACTS</a></li>
+                <i class="fas fa-search"></i>
+            </ul>
+        </nav>
+    </header>
+    <section class="main-section">
+        <div class="center-div">
+            <span class='host-title'>HOSTING SERVICES</span>
+            <br>
+            KRAS<span class='title'>HOSTING</span>
+        </div>
+    </section>
+    <section class='our-program'>
+        <div>
+            <h1 class='our-program-text'>Our Program</h1>
+        </div>
+    </section>
+    <section class='other-section'>
+        <div>
+            
+        </div>
+    </section>
+
+    <script src="https://kit.fontawesome.com/19b050893f.js" crossorigin="anonymous"></script>
+    <script src="src/script.js"></script>
+</body>
+</html>
